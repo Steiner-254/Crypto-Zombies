67363dcae7de9dcc92b06fc01a42816a445e1084
Steiner254 <70921705+Steiner-254@users.noreply.github.com>
2024-05-06 06:25:41 -0400

    Update zombiefeeding.sol

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -5,5 +5,10 @@ pragma solidity ^0.8.0;
 import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
+// storage vs memory
+function feedAndMultiply(uint _zombieId, uint _targetDNA) public {
+require(msg.sender == zombieToOwner[_zombieId]);
+Zombie storage myZombie = zombies[_zombieId]
+}
 
 }
