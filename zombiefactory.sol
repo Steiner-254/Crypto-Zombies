7996f4266560da49a8304df6ee303d1f07581a68
Steiner254 <70921705+Steiner-254@users.noreply.github.com>
2024-05-06 03:53:48 -0400

    Update zombiefactory.sol

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -20,13 +20,15 @@ Zombie[] public zombies;
 function _createZombie(string _name, uint _dna) private {
 // working with structs and arrays
 zombie.push(Zombie(_name, _dna));
+}
 
 // private view returns function
 function _generateRandomDna(string _str) private view returns (uint)
 // Keccak256 & Typecasting
 uint rand = uint (keccak256(_str));
 return rand % dnaModulus;
-
 }
+
+
 
 }
